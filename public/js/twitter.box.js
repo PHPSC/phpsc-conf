bbcfd9353c69292327a8d0ee2bfee0b924a2a03c
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2012-07-13 13:32:32 -0300

    Adição de script para busca de tweets via Javascript

---
+++ public/js/twitter.box.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function() {
+	$.ajax(
+		{
+			url: 'https://search.twitter.com/search.json',
+			type: 'GET',
+			data: {
+				q: '%23phpscconf%20OR%20%40PHP_SC',
+				rpp: 15,
+				result_type: 'mixed'
+			},
+			dataType: 'jsonp',
+			success: function(data)
+			{
+				alert(data.results.length);
+			}
+		}
+	);
+});*/
