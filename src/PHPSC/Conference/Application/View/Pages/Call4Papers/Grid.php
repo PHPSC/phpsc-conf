a35c29c54de85afd3b2481de16f77d37b38200c9
Rafael Kassner <kassner@gmail.com>
2012-07-13 11:48:04 -0300

    Refactoring Task listing

--- src/PHPSC/Conference/Application/View/Pages/Call4Papers/Grid.php
+++ src/PHPSC/Conference/Application/View/Pages/Call4Papers/Grid.php
@@ -1,9 +1,11 @@
 <?php
 namespace PHPSC\Conference\Application\View\Pages\Call4Papers;
 
-use PHPSC\Conference\Application\View\Main;
+use \PHPSC\Conference\Application\View\Main;
 use \PHPSC\Conference\Domain\Entity\Event;
 use \Lcobucci\DisplayObjects\Core\UIComponent;
+use \Lcobucci\DisplayObjects\Components\Datagrid\DatagridColumn;
+use \Lcobucci\DisplayObjects\Components\Datagrid\Datagrid;
 
 class Grid extends UIComponent
 {
@@ -26,5 +28,22 @@ class Grid extends UIComponent
     public function getTalks()
     {
         return $this->talks;
+    }
+    
+    /**
+     * @return string|\Lcobucci\DisplayObjects\Components\Datagrid\Datagrid
+     */
+    public function getDatagrid()
+    {
+    	$datagrid = new Datagrid('talks', $this->getTalks(), array(
+    		new DatagridColumn('#', 'id'),
+    		new DatagridColumn('Nome', 'title'),
+    		new DatagridColumn('Descrição Curta', 'shortDescription'),
+    		new DatagridColumn('Aprovada?', 'approved', '', function($value){
+    			return ($value ? 'Sim' : 'Não');
+    		}),
+    	));
+    	$datagrid->setStyleClass('table table-striped');
+    	return $datagrid;
     }
 }
