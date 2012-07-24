fa878e39ca711dd190fb777554928ee4050287f1
paruckerr <paruckerr@gmail.com>
2012-07-24 18:58:14 -0300

    Criação do Map com latitude e longitude. (refs #4)

---
+++ public/js/venue.map.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+	
+	
+	var location = new google.maps.LatLng(lat,lng);
+	
+	var mapContato = new google.maps.Map(
+		document.getElementById('mapEvento'),
+		{
+			zoom:11,
+			center: location,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			mapTypeControl:false,
+			disableDefaultUI: true,
+			navigationControl: true,
+			navigationControlOptions: {
+				style: google.maps.NavigationControlStyle.SMALL
+			}
+		}
+	);
+
+	var icon = new google.maps.MarkerImage(baseUrl + '/img/pin.png',
+	      	new google.maps.Size(62, 80),
+	      	new google.maps.Point(0, 0)
+	);
+
+	new google.maps.Marker(
+		{
+			position: location,
+			title: 'Local do Evento',
+			animation: google.maps.Animation.DROP,
+			map: mapContato,
+			icon: icon, 
+		}
+	);
+
+	geocodePosition(location);
+});
+
+function geocodePosition(location) 
+{
+	var geocoder = new google.maps.Geocoder();
+	
+    geocoder.geocode({
+        latLng: location
+    }, function (responses) {
+        if (responses && responses.length > 0) {
+        	updateMarkerStatus(responses[0].formatted_address);
+        	} else {
+        		updateMarkerStatus(';( Endereço não encontrado.');
+        }
+    });
+}
+
+function updateMarkerStatus(local) {
+	var address = local.toString().split(',');
+	document.getElementById('address').innerHTML = '<i class="icon-map-marker"></i> ' + address[0];
+}
--- src/PHPSC/Conference/Application/View/Pages/Venue.php
+++ src/PHPSC/Conference/Application/View/Pages/Venue.php
@@ -1,8 +1,15 @@
 <?php
 namespace PHPSC\Conference\Application\View\Pages;
 
+use PHPSC\Conference\Application\View\Main;
+
 use \Lcobucci\DisplayObjects\Core\UIComponent;
 
 class Venue extends UIComponent
 {
+    public function __construct()
+    {
+        Main::appendScript('http://maps.googleapis.com/maps/api/js?sensor=false');
+        Main::appendScript($this->getBaseUrl() . '/js/venue.map.js');
+    }
 }
