7c99f386b8ade1c5950ac2616a3f365fe084e03d
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-08-25 14:41:26 -0300

    jQuery updated (#22)

--- src/PHPSC/Conference/Application/View/Main.php
+++ src/PHPSC/Conference/Application/View/Main.php
@@ -51,7 +51,7 @@ class Main extends UIComponent
         $this->content = $content;
 
         static::appendScript($this->getUrl('js/bootstrap.min.js'), true);
-        static::appendScript($this->getUrl('js/jquery-1.7.2.min.js'), true);
+        static::appendScript($this->getUrl('js/jquery-1.10.2.min.js'), true);
     }
 
     /**
