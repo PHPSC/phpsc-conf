1df8f3945b9ca5ecd0b958f05eff8b7d18f561e9
Domingos Teruel <mingomax@gmail.com>
2013-08-31 21:33:38 -0300

    - Diagramação da HOME - Removido a Sidebar Patrocionio e Apoio, movido para o rodapé - Refatorado HTML e CSS do Rodapé - Adicionado icones glyphonics-social (Licenca CC) - Refatorado Estrutura HTML dos Patrociandores, agora pode-se adicionar 3 por linhas - Rodapé Sobre refatorado. - Página Sobre refatorada.

--- src/PHPSC/Conference/UI/Footer.php
+++ src/PHPSC/Conference/UI/Footer.php
@@ -5,4 +5,11 @@ use \Lcobucci\DisplayObjects\Core\UIComponent;
 
 class Footer extends UIComponent
 {
+	/**
+	 * @return \PHPSC\Conference\UI\Sponsors
+	 */
+	public function renderSponsors()
+	{
+		return new Sponsors();
+	}
 }
