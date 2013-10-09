1a730faee70f9812d28e55566bba09ffb5d3f6ff
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-10-08 22:35:02 -0300

    Log path changed

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/../phperrors.log');
+ini_set('error_log', __DIR__ . '/../tmp/phperrors.log');
 ini_set('session.save_path', __DIR__ . '/../tmp/session');
 
 require __DIR__ . '/../boot.php';
