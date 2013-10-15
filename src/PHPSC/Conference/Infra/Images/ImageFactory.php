cd3851df434fe80b45366855349ae89c85a9bf1d
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-10-14 21:09:34 -0300

    Workarround to Imagick 2.3.0

--- src/PHPSC/Conference/Infra/Images/ImageFactory.php
+++ src/PHPSC/Conference/Infra/Images/ImageFactory.php
@@ -23,9 +23,39 @@ class ImageFactory
      */
     public function createFromResource($resource)
     {
+        if ($this->isImagick2Dot3()) {
+            return $this->createFromBlob(stream_get_contents($resource));
+        }
+
         $image = new Imagick();
         $image->readImageFile($resource);
 
         return $image;
+    }
+
+    /**
+     * @param string $content
+     * @return Imagick
+     */
+    public function createFromBlob($content)
+    {
+        $image = new Imagick();
+        $image->readImageBlob($content);
+
+        return $image;
+    }
+
+    /**
+     * The imagick module version 2.3.0 has a bug when loading PNGs from the resource
+     *
+     * @link https://bugs.php.net/bug.php?id=58948
+     * @return boolean
+     */
+    protected function isImagick2Dot3()
+    {
+        $version = exec('php -i | grep "imagick module version"');
+        $version = substr($version, strpos($version, '=>') + 3);
+
+        return $version == '2.3.0';
     }
 }
