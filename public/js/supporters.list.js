50f1ef0106da6dcd6eb18b89e6205d2234ff8e42
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-10-18 07:58:22 -0300

    Supporter logos should be vertically aligned

--- public/js/supporters.list.js
+++ public/js/supporters.list.js
@@ -14,15 +14,15 @@ $(document).ready(function () {
 				for (var i = 0; i < supporters.length; ++i) {
 					var supporter = supporters[i];
 					
-					if (i % 6 == 0) {
+					if (i % 6 == 0 && element.children().length > 0) {
 						$('#supportersContainer').append(element);
 						element = $('<div class="row"></div>');
 					}
 					
 					element.append(
-						'<div class="col-md-2" style="padding-top: 10px; text-align: center;">'
+						'<div class="col-md-2" style="padding-top: 10px; text-align: center; line-height: 80px;">'
 					    	+ '<a href="' + supporter.website + '" target="_blank" title="' + supporter.name + '">'
-				        		+ '<img src="' + baseUrl + 'supporter/' + supporter.id + '?w=180&h=95" alt="' + supporter.name + '">'
+				        		+ '<img src="' + baseUrl + 'supporter/' + supporter.id + '?w=160&h=80" alt="' + supporter.name + '">'
 		        			+ '</a>'
 						+ '</div>'
 					);
