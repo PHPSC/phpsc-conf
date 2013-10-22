314d57f8d9a50be04988c373dbf3986f1f0451d7
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-10-22 06:54:54 -0200

    Added room information to allocate the talks on a location

---
+++ src/PHPSC/Conference/Domain/Entity/Room.php
@@ -0,0 +1,91 @@
+<?php
+namespace PHPSC\Conference\Domain\Entity;
+
+use InvalidArgumentException;
+
+/**
+ * @Entity()
+ * @Table("room")
+ * @author Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
+ */
+class Room
+{
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @generatedValue(strategy="IDENTITY")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Column(type="string", length=80)
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ManyToOne(targetEntity="Location")
+     * @JoinColumn(name="location_id", referencedColumnName="id")
+     * @var Location
+     */
+    private $location;
+
+    /**
+     * @return number
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param number $id
+     */
+    public function setId($id)
+    {
+        if ($id <= 0) {
+            throw new InvalidArgumentException(
+                'O id deve ser maior ou igual à ZERO'
+            );
+        }
+
+        $this->id = (int) $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        if (empty($name)) {
+            throw new InvalidArgumentException('O nome não pode ser vazio');
+        }
+
+        $this->name = (string) $name;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param Location $location
+     */
+    public function setLocation(Location $location)
+    {
+        $this->location = $location;
+    }
+}
