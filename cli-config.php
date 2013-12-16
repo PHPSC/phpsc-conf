b0da767a68b362dcfb4cab7663d0665841c9b21f
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-12-16 19:04:00 -0200

    Moving DI container configuration to a single place

--- cli-config.php
+++ cli-config.php
@@ -1,16 +1,23 @@
 <?php
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
+use Lcobucci\ActionMapper2\DependencyInjection\ContainerConfig;
 use Lcobucci\DependencyInjection\XmlContainerBuilder;
 use Symfony\Component\Console\Helper\HelperSet;
 
+$config = require __DIR__ . '/config/di-container.php';
+
 $builder = new XmlContainerBuilder(
-    'PHPSC\Conference\Infra\DependencyInjection\Container',
-    __DIR__ . '/tmp'
+    ContainerConfig::getClass($config),
+    ContainerConfig::getDumpDir($config)
 );
 
-$container = $builder->getContainer(__DIR__ . '/config/services.xml');
-$container->getParameterBag()->set('app.baseDir', __DIR__ . '/');
+$container = $builder->getContainer(
+    $config->getFile(),
+    array(),
+    $config->getDefaultParameters()
+);
+
 $em = $container->get('entitymanager');
 
 return new HelperSet(
