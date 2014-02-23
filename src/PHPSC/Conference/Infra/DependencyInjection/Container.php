a4bc9df3b99810dd3ea821dc03d6c9000f16cdca
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2014-02-23 19:26:19 -0300

    Adding dev mode to dependency injection container (closes #70)

--- src/PHPSC/Conference/Infra/DependencyInjection/Container.php
+++ src/PHPSC/Conference/Infra/DependencyInjection/Container.php
@@ -9,7 +9,6 @@ use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Lcobucci\Fixture\Persistence\EntityManagerEventSubscriber;
-use Doctrine\Fixture\Executor\BulkExecutorEventSubscriber;
 
 /**
  * @codingStandardsIgnoreFile
@@ -57,17 +56,9 @@ class Container extends \Lcobucci\ActionMapper2\DependencyInjection\Container
         $instance->setQueryCacheImpl($cache);
         $instance->setResultCacheImpl($cache);
 
-        $instance->setProxyDir(
-            $baseDir . $this->getParameter('doctrine.proxy.dir')
-        );
-
-        $instance->setProxyNamespace(
-            $this->getParameter('doctrine.proxy.namespace')
-        );
-
-        $instance->setAutoGenerateProxyClasses(
-            $this->getParameter('doctrine.proxy.auto')
-        );
+        $instance->setProxyDir($baseDir . $this->getParameter('doctrine.proxy.dir'));
+        $instance->setProxyNamespace($this->getParameter('doctrine.proxy.namespace'));
+        $instance->setAutoGenerateProxyClasses($this->getParameter('app.devmode'));
 
         AnnotationRegistry::registerFile(
             $baseDir . 'vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
@@ -92,6 +83,7 @@ class Container extends \Lcobucci\ActionMapper2\DependencyInjection\Container
     protected function getFixtures_ConfigService()
     {
         $config = new \Doctrine\Fixture\Configuration();
+
         $config->getEventManager()->addEventSubscriber(
             new EntityManagerEventSubscriber($this->get('entityManager'))
         );
@@ -105,6 +97,65 @@ class Container extends \Lcobucci\ActionMapper2\DependencyInjection\Container
     protected function getFixtures_LoaderService()
     {
         return new DirectoryLoader(__DIR__ . '/../Fixtures');
+    }
+
+    /**
+     * @return \Doctrine\Common\Cache\CacheProvider
+     */
+    protected function getApp_CacheService()
+    {
+        $this->services['app.cache'] = $cache = $this->getCacheDriver();
+
+        $cache->setNamespace($this->getParameter('cache.prefix'));
+
+        return $cache;
+    }
+
+    /**
+     * @return \Doctrine\Common\Cache\CacheProvider
+     */
+    protected function getCacheDriver()
+    {
+        if ($this->getParameter('app.devmode')) {
+            return new \Doctrine\Common\Cache\ArrayCache();
+        }
+
+        if (!$this->hasParameter('cache.host')) {
+            return new \Doctrine\Common\Cache\ApcCache();
+        }
+
+        $memcached = new \Memcached();
+        $memcached->addServer(
+            $this->getParameter('cache.host'),
+            $this->getParameter('cache.port')
+        );
+
+        $cache = new \Doctrine\Common\Cache\MemcachedCache();
+        $cache->setMemcached($memcached);
+
+        return $cache;
+    }
+
+    /**
+     * @return \Swift_Transport
+     */
+    protected function getSwiftmailer_TransportService()
+    {
+        if ($this->getParameter('app.devmode')) {
+            return $this->services['swiftmailer.transport'] = \Swift_NullTransport::newInstance();
+        }
+
+        $transport = \Swift_SmtpTransport::newInstance(
+        	$this->getParameter('smtp.host'),
+        	$this->getParameter('smtp.port'),
+        	$this->getParameter('smtp.encryption')
+        );
+
+        $transport->setUsername($this->getParameter('smtp.username'));
+        $transport->setPassword($this->getParameter('smtp.password'));
+        $transport->setAuthMode($this->getParameter('smtp.auth_mode'));
+
+        return $this->services['swiftmailer.transport'] = $transport;
     }
 
     /**
