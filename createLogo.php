be1f5b5078c9fa2d047a796bff438f4071b03857
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2014-06-19 21:08:34 +0000

    Fixing the DI container usage

--- createLogo.php
+++ createLogo.php
@@ -1,22 +1,8 @@
 <?php
 require __DIR__ . '/boot.php';
 
-use Lcobucci\DependencyInjection\XmlContainerBuilder;
-use Lcobucci\ActionMapper2\DependencyInjection\ContainerConfig;
 use PHPSC\Conference\Domain\Entity\Logo;
 
-$config = require __DIR__ . '/config/di-container.php';
-
-$builder = new XmlContainerBuilder(
-    ContainerConfig::getClass($config),
-    ContainerConfig::getDumpDir($config)
-);
-
-$container = $builder->getContainer(
-    $config->getFile(),
-    array(),
-    $config->getDefaultParameters()
-);
 
 if (!isset($argv[1]) || !file_exists($argv[1])) {
     echo 'Valid image path must be informed' . PHP_EOL;
@@ -27,6 +13,7 @@ $logo = new Logo();
 $logo->setImage(fopen($argv[1], 'rb'));
 $logo->setCreatedAt(new \DateTime());
 
+$container = require __DIR__ . '/config/di-container.php';
 $em = $container->get('entityManager');
 
 $em->persist($logo);
